a4187161dde261e1d08d7dfa35ac93c7af99b009
li_lubo <li_lubo@aliyun.com>
2021-02-23 16:40:51 +0800

    定义模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+"""
+1. 模型类 需要继承自 models.Model
+2. 系统会自动添加一个主键 id
+3. 字段
+    字段名 = model.类型（选项）
+    
+    字段名就是数据表的字段名
+    字段名不要使用python、mysql的关键字
+    
+    char(M)
+    varchar(M)
+    M 就是选项
+"""
+
+
+class BookInfo(models.Model):
+    # id
+    name = models.CharField(max_length=10)
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+
+    # 外键约束
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
