a6a0cafacf939299a92892b76db11fe189ae758d
li_lubo <li_lubo@aliyun.com>
2021-02-25 10:52:38 +0800

    子应用urls

---
+++ bookmanager/book/urls.py
@@ -0,0 +1,13 @@
+# 李禄波
+# 2021/2/25 10:32
+
+from django.urls import path
+from .views import index
+
+# 要访问子应用里的路由需要通过 工程路由 + 子应用路由
+# 固定写法 urlpatterns = []
+urlpatterns = [
+    # path(路由, 视图函数名)
+    path('index/', index)
+
+]
--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../book'))
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # path(路由， 视图函数名)
+    # path('index/', views.index),
+    path('', include('book.urls'))
 ]
