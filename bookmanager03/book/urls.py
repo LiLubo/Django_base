ad6d17005cd7d8834da6854bb9b6cb33c3fb5a92
li_lubo <li_lubo@aliyun.com>
2021-02-28 10:33:21 +0800

    'Http向服务器传参'

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,7 +1,10 @@
 # 李禄波
 # 2021/2/27 17:02
 from django.urls import path
-from book.views import create_book
+from book.views import *
 urlpatterns = [
     path('create/', create_book),
+
+    # 获取路径中的参数
+    path('<city_id>/<shop_id>/', shop),
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -12,3 +12,31 @@ def create_book(request):
         readcount=1000,
     )
     return HttpResponse('create')
+
+
+def shop(request, city_id, shop_id):
+    """
+    查询字符串
+    http://ip:port/path/path/?key=value&key1=value
+
+    url 以 ? 为分隔
+    ?之前是 请求路径
+    ?之后是 查询字符串  类似于字典
+    """
+    # 获取查询字符串
+    # <QueryDict: {'order': ['lalala']}>
+    # QueryDict具有字典的特性 且可以一键多值
+    query_parms = request.GET
+    print(query_parms)
+
+    # 当一键多值时，获取键值需要使用getlist(),否则只能得到最后一个键值
+    order = query_parms.getlist('order')
+
+    # order = query_parms.get('order')
+    # order = query_parms['order']
+    print(order)
+    return HttpResponse('禄波的小商店')
+
+
+
+
